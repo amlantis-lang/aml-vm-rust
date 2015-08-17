36f42d64258e86e6919ceb180d0f8c5b4203217b
Markéta Lisová <marketa@lisova.cz>
2015-08-18 01:32:16 +0200

    Adding some Rust string magic

--- gearvm/build.rs
+++ gearvm/build.rs
@@ -8,8 +8,8 @@ fn main() {
         .unwrap_or_else(|e| {
             panic!("Failed to execute: {}", e)
         });
-    let icu_libs = String::from_utf8_lossy(&icu_libs_output.stdout)
-        .to_string()[2 .. icu_libs_output.stdout.len()]
+    let icu_libs = &String::from_utf8_lossy(&icu_libs_output.stdout)
+        [2 .. icu_libs_output.stdout.len()]
         .to_string();
     println!("cargo:rustc-link-search=native={}", icu_libs);
     println!("cargo:rustc-link-lib=dylib=icui18n");
