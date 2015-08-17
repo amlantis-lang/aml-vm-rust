121a1febb86ce6ad8f2e9f9423b599e63ad9132d
Markéta Lisová <marketa@lisova.cz>
2015-08-18 01:36:07 +0200

    Bit of refactoring in the build script

--- gearvm/build.rs
+++ gearvm/build.rs
@@ -2,18 +2,21 @@ use std::process::Command;
 use std::string::String;
 
 fn main() {
+    println!("cargo:rustc-link-search=native={}", icu_libs_path());
+    println!("cargo:rustc-link-lib=dylib=icui18n");
+    println!("cargo:rustc-link-lib=dylib=icuuc");
+    println!("cargo:rustc-link-lib=dylib=icudata");
+    println!("cargo:rustc-link-lib=dylib=icuio");
+}
+
+fn icu_libs_path() -> String {
     let icu_libs_output = Command::new("pkg-config")
         .args(&["--libs-only-L", "icu-uc", "icu-io"])
         .output()
         .unwrap_or_else(|e| {
             panic!("Failed to execute: {}", e)
         });
-    let icu_libs = &String::from_utf8_lossy(&icu_libs_output.stdout)
+    String::from_utf8_lossy(&icu_libs_output.stdout)
         [2 .. icu_libs_output.stdout.len()]
-        .to_string();
-    println!("cargo:rustc-link-search=native={}", icu_libs);
-    println!("cargo:rustc-link-lib=dylib=icui18n");
-    println!("cargo:rustc-link-lib=dylib=icuuc");
-    println!("cargo:rustc-link-lib=dylib=icudata");
-    println!("cargo:rustc-link-lib=dylib=icuio");
+        .to_string()
 }
